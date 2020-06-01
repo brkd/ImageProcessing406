6bf09a852b72c67fbe628ca8085b8e39235a3d1e
brkd <berkayd1812@gmail.com>
2020-06-02 02:37:14 +0300

    Median filter works properly.

--- GPU/gpimprocess.cu
+++ GPU/gpimprocess.cu
@@ -2,18 +2,18 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cmath>
 
-#define TILE_W 16
-#define TILE_H 16
+
+#define TILE_W 32
+#define TILE_H 32
 #define R 1
 #define D ((R*2)+1)
 #define S (D*D)
-#define BLOCK_W TILE_W
-#define BLOCK_H TILE_H
 
 
 //For debug purposes
-void printImageMatrix(float** image, int height, int width)
+void printImageMatrix(unsigned char** image, int height, int width)
 {
   for(int i = 0; i < height; i++)
     {
@@ -26,9 +26,9 @@ void printImageMatrix(float** image, int height, int width)
 }
 
 
-float* flattenImage(float** image, int height, int width)
+unsigned char* flattenImage(unsigned char** image, int height, int width)
 {
-   float* image_1D = new float[height*width];
+   unsigned char* image_1D = new unsigned char[height*width];
    for(int i = 0; i < height; i++)
       {
          for(int j = 0; j < width; j++)
@@ -41,7 +41,7 @@ float* flattenImage(float** image, int height, int width)
 }
 
 
-void fileToMatrix(float** &image, char* fileName, int* height, int* width)
+void fileToMatrix(unsigned char** &image, char* fileName, int* height, int* width)
 {
    std::ifstream imageFile(fileName);
 
@@ -53,15 +53,15 @@ void fileToMatrix(float** &image, char* fileName, int* height, int* width)
 	iss >> *(height) >> *(width);
 	std::cout << "Height: " << *height << " ||-|| Width: " << *width << std::endl;
 
-	image = new float*[*(height)];
+	image = new unsigned char*[*(height)];
 
 	for(int i = 0; i < *(height); i++)
 	   {
-              image[i] = new float[*(width)];
+              image[i] = new unsigned char[*(width)];
            }	  
 
       	int h = 0;
-      	float val;
+      	int val;
         while(getline(imageFile, line))
            {
               int w = 0;
@@ -78,7 +78,7 @@ void fileToMatrix(float** &image, char* fileName, int* height, int* width)
     imageFile.close();      
 }
 
-void matrixToFile(char* fileName, float* image, int height, int width)
+void matrixToFile(char* fileName, unsigned char* image, int height, int width)
 {
   std::ofstream outFile;
   outFile.open(fileName);
@@ -89,14 +89,14 @@ void matrixToFile(char* fileName, float* image, int height, int width)
 	int y = i / width;
 	if(i != 0 && x == 0)
 	   outFile << '\n';
-        outFile << ("%.2f", image[x + y*width]) << " ";
+        outFile << int(image[x + y*width]) << " ";
     }
 
   outFile.close();
 }
 
 
-void getMinAndMax(float** image, int height, int width, int* min, int* max)
+void getMinAndMax(unsigned char** image, int height, int width, int* min, int* max)
 {
    for(int i = 0; i < height; i++)
       {
@@ -125,60 +125,84 @@ __global__ void linearScale(float* image, int* a, int* width, float* b)
 }
 
 
-__global__ void medianFilter(float* image, float* outImage, int* height, int* width)
+__global__ void medianFilter(unsigned char* image, unsigned char* outImage, int* height, int* width)
 {
-   int x = blockIdx.x * TILE_W + threadIdx.x;
-   int y = blockIdx.y * TILE_H + threadIdx.y;
+   int im_y = blockIdx.y * blockDim.y + threadIdx.y;
+   int im_x = blockIdx.x * blockDim.x + threadIdx.x; 	
+	
+   if(im_y == 0 || im_y == (*height) - 1 || im_x == 0 || im_x == (*width) - 1)
+   {
+      outImage[im_y * (*width) + im_x] = 0;
+      return;
+   }
 
-   unsigned int index = y * (*width) + x;
-   unsigned int block_index = (threadIdx.y * blockDim.y + threadIdx.x);
+
+   int index = im_y * (*width) + im_x;//(((blockIdx.x) + 1) * (*width)) + threadIdx.x + 1;
+   if(index >= (*height) * (*width))
+      return;
+   int image_index;
    
-   __shared__ float sharedWindow[BLOCK_W * BLOCK_H];
-   sharedWindow[block_index] = image[index];
+   __shared__ unsigned char window[S];
+   int window_index = 0;
 
-   __syncthreads();
+   for(int i = -1; i <= 1; i++)
+   {
+      for(int j = -1; j <= 1; j++)
+      {
+         image_index = index + (i * (*width)) + j;
+	 window[window_index++] = image[image_index];
+      }
+   }
 
-  int i, key, j;
-  for (i = 1; i < block_index; i++)
-     {
-       key = sharedWindow[i];
-       j = i - 1;
+   int key, i ,j;
 
-       while (j >= 0 && sharedWindow[j] > key)
-         {
-           sharedWindow[j + 1] = sharedWindow[j];
-           j = j - 1;
-         }
-       sharedWindow[j + 1] = key;
-     }
+   for(i = 1; i < S; i++)
+   {
+      key = window[i];
+      j = i - 1;
 
-   outImage[index] = sharedWindow[block_index / 2];
+      while(j >= 0 && window[j] > key)
+      {
+         window[j + 1] = window[j];
+	 j = j - 1;
+      }
+
+      window[j + 1] = key;
+   }
+
+   outImage[index] = window[S/2];
 }
+
 
 int main(int argc, char** argv)
 {
    char* inFileName = argv[1];
    char* outFileName = argv[2];
    int writeOut = atoi(argv[3]);
+   std::cout << "BEFORE CUDA EXECUTION" << std::endl;
+   cudaEvent_t start, stop;
+   cudaEventCreate(&start);
+   cudaEventCreate(&stop);
 
+   std::cout << "STARTED EXECUTION" << std::endl;
 
    //IMAGE INIT
    int height, width, max = -1, min = 256;
-   float** hostImage;
+   unsigned char** hostImage;
 
    fileToMatrix(hostImage, inFileName, &height, &width);
    getMinAndMax(hostImage, height, width, &min, &max);
-   float* hostFlattened = flattenImage(hostImage, height, width);  
-
+   unsigned char* hostFlattened = flattenImage(hostImage, height, width);  
+   //printImageMatrix(hostImage, height, width);
    //GPU INIT
-   const size_t image_size = sizeof(float) * size_t(height*width);
+   const int image_size = sizeof(unsigned char) * (height*width);
 
-   float* hostResult = new float[height*width];
-   float* deviceImage;
+   unsigned char* hostResult = new unsigned char[height*width];
+   unsigned char* deviceImage;
    int *height_d, *width_d;
 
-   cudaMalloc((void**)&height_d, sizeof(int));
-   cudaMalloc((void**)&width_d, sizeof(int));   
+   cudaMalloc(&height_d, sizeof(int));
+   cudaMalloc(&width_d, sizeof(int));   
 
    cudaMemcpy(height_d, &height, sizeof(int), cudaMemcpyHostToDevice);         
    cudaMemcpy(width_d, &width, sizeof(int), cudaMemcpyHostToDevice);         
@@ -191,37 +215,56 @@ int main(int argc, char** argv)
    float gmax = 255.0;
    float b = gmax / (max - min), *d_b;
 
-   cudaMalloc((void**)&d_a, sizeof(int));
-   cudaMalloc((void**)&d_b, sizeof(float));
+   //cudaMalloc(&d_a, sizeof(int));
+   //cudaMalloc(&d_b, sizeof(float));
 
-   cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
-   cudaMemcpy(d_b, &b, sizeof(float), cudaMemcpyHostToDevice);
+   //cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+   //cudaMemcpy(d_b, &b, sizeof(float), cudaMemcpyHostToDevice);
 
-   cudaMalloc((void**)&deviceImage, image_size);
-   cudaMemcpy(deviceImage, hostFlattened, image_size, cudaMemcpyHostToDevice);	
+   //cudaMalloc(&deviceImage, image_size);
+   //cudaMemcpy(deviceImage, hostFlattened, image_size, cudaMemcpyHostToDevice);	
 
-   linearScale<<<BLOCK_C, 1>>>(deviceImage, d_a, width_d, d_b);
-   cudaMemcpy(hostResult, deviceImage, image_size, cudaMemcpyDeviceToHost);
+   //linearScale<<<BLOCK_C, 1>>>(deviceImage, d_a, width_d, d_b);
+   //cudaMemcpy(hostResult, deviceImage, image_size, cudaMemcpyDeviceToHost);
 
-   if(writeOut)
-      matrixToFile(outFileName, hostResult, height, width);
+   //if(writeOut)
+     // matrixToFile(outFileName, hostResult, height, width);
    
 
-   /*MEDFILTER
-   dim3 grid(TILE_W, TILE_H);
-   dim3 block(BLOCK_W, BLOCK_H);
+   //MEDFILTER
 
-   float* outImage;
-   cudaMalloc((void**)&outImage, image_size);	
+   unsigned char* outImage;
+   cudaMalloc(&outImage, image_size);	
 
-   cudaMalloc((void**)&deviceImage, image_size);
+   cudaMalloc(&deviceImage, image_size);
    cudaMemcpy(deviceImage, hostFlattened, image_size, cudaMemcpyHostToDevice);
 
-   medianFilter<<<grid, block>>>(deviceImage, outImage, height_d, width_d);
+
+   int BLOCK_H = (int)std::ceil((float)width / TILE_H);
+   int BLOCK_W = (int)std::ceil((float)height / TILE_W);
+   std::cout << BLOCK_H << " " << BLOCK_W << std::endl;
+
+   dim3 deviceBlocks(BLOCK_H, BLOCK_W);
+   
+
+
+   dim3 deviceThreads(32, 32);
+
+   cudaEventRecord(start);
+   medianFilter<<<deviceBlocks, deviceThreads>>>(deviceImage, outImage, height_d, width_d);
+   cudaEventRecord(stop);
+
+   cudaEventSynchronize(stop);
+   cudaGetLastError();
+   
+   float elapsed = 0;
+   cudaEventElapsedTime(&elapsed, start, stop);
+   
+   std::cout << "MEDIAN FILTER CALCULATION: " << elapsed / 1000<< std::endl;
    cudaMemcpy(hostResult, outImage, image_size, cudaMemcpyDeviceToHost);
 
    if(writeOut)
-      matrixToFile(outFileName, hostResult, height, width);*/
+      matrixToFile(outFileName, hostResult, height, width);
 
    return 0;
 }
